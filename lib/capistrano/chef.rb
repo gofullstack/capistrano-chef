b488a8f8bff018a98e3f4cc5af048aa35ad8ab30
Nathan L Smith <nlloyds@gmail.com>
2012-03-26 22:38:06 -0500

    fix unintentional debug mode for knife

--- lib/capistrano/chef.rb
+++ lib/capistrano/chef.rb
@@ -3,6 +3,14 @@ require 'chef/knife'
 require 'chef/search/query'
 
 module Capistrano::Chef
+  # Set up chef configuration
+  def self.configure_chef
+    knife = Chef::Knife.new
+    # If you don't do this it gets thrown into debug mode
+    knife.config = { :verbosity => 1 }
+    knife.configure_chef
+  end
+
   # Do a search on the Chef server and return an attary of the requested
   # matching attributes
   def self.search_chef_nodes(query = '*:*', options = {})
@@ -14,7 +22,7 @@ module Capistrano::Chef
 
   # Load into Capistrano
   def self.load_into(configuration)
-    Chef::Knife.new.configure_chef
+    self.configure_chef
     configuration.set :capistrano_chef, self
     configuration.load do
       def chef_role(name, query = '*:*', options = {})
